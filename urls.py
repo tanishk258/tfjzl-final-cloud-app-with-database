bb1778830ad6ff4b11e7bd36c9dad35dda04dfe7
TANISHK SINGH <tanishksingh4444@gmail.com>
2026-06-21 09:34:10 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,1 +1,20 @@
+```python
+from django.urls import path
+from . import views
+
+app_name = 'onlinecourse'
+
+urlpatterns = [
+    path(
+        'submit/<int:course_id>/',
+        views.submit,
+        name='submit'
+    ),
+    path(
+        'show_exam_result/<int:course_id>/<int:submission_id>/',
+        views.show_exam_result,
+        name='show_exam_result'
+    ),
+]
+```
 
